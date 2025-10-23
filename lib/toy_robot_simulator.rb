cbcd589230e87105676dfd536e2cedb0f44965a8
zerga9 <gadizaz@hotmail.com>
2025-10-23 20:32:53 +0100

    Add Simulator to coordinate robot placement on table

--- lib/toy_robot_simulator.rb
+++ lib/toy_robot_simulator.rb
@@ -3,6 +3,7 @@
 require_relative '../lib/toy_robot_simulator/command_parser'
 require_relative '../lib/toy_robot_simulator/position'
 require_relative '../lib/toy_robot_simulator/robot'
+require_relative '../lib/toy_robot_simulator/simulator'
 require_relative '../lib/toy_robot_simulator/table'
 
 module ToyRobotSimulator
---
+++ lib/toy_robot_simulator/simulator.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module ToyRobotSimulator
+  class Simulator
+    attr_reader :robot, :table
+
+    def initialize
+      @table = Table.new
+      @robot = Robot.new(@table)
+    end
+
+    def execute(command_hash)
+      return if command_hash.nil?
+
+      case command_hash[:command]
+      when :place
+        place_robot(command_hash[:x], command_hash[:y], command_hash[:direction])
+      end
+    end
+
+    private
+
+    def place_robot(x, y, direction)
+      position = Position.new(x, y)
+      return unless @table.valid_position?(position)
+
+      @robot.place(position, direction)
+    end
+  end
+end
