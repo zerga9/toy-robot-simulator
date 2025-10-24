3c02c8952469ab6787b83c5c4e3835ebe7f1586e
zerga9 <gadizaz@hotmail.com>
2025-10-24 03:51:14 +0100

    Remove attr_readers from CLI for better encapsulation

--- lib/toy_robot_simulator/cli.rb
+++ lib/toy_robot_simulator/cli.rb
@@ -4,8 +4,6 @@ require_relative 'usage_text'
 
 module ToyRobotSimulator
   class CLI
-    attr_reader :args, :parser, :simulator
-
     def initialize(args)
       @args = args
       @parser = CommandParser.new
@@ -13,13 +11,13 @@ module ToyRobotSimulator
     end
 
     def run
-      print_usage if args.empty?
+      print_usage if @args.empty?
 
       create_reader.read do |line|
-        cmd = parser.parse(line)
+        cmd = @parser.parse(line)
         break if cmd && cmd[:command] == :exit
 
-        simulator.execute(cmd)
+        @simulator.execute(cmd)
       end
     end
 
@@ -30,7 +28,7 @@ module ToyRobotSimulator
     end
 
     def create_reader
-      args.empty? ? Readers::StdinReader.new : Readers::FileReader.new(args[0])
+      @args.empty? ? Readers::StdinReader.new : Readers::FileReader.new(@args[0])
     end
   end
 end
